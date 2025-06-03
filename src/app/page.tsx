0811c0a43213e6d616a3cbf8eacf8fad5f808363
Mehmet Salih Kuscu <37173211+MehmetSalihK@users.noreply.github.com>
2025-06-03 17:19:19 +0200

    removed @vercel/analystics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   const [isLive, setIsLive] = useState(false);
